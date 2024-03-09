ea9bd32d9e2c5a5153b5740f3a480170dc14cda2
jonie_mirc <jonie.mirc@gmail.com>
2024-03-08 18:02:07 -0600

    Codigo desarrollado segundo parcial, arreglos, clases y objetos

---
+++ CalificacionesConPOO/CalificacionesConPOO/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace CalificacionesConPOO {
+    public class Program { 
+        public static void Main(string[] args)
+        {
+            //Declarar un arreglo de estudiantes
+            Estudiante[] estudiantes;
+            Console.WriteLine("Ingrese la cantidad de estudiantes: ");
+            int cantidadEstudiantes = Convert.ToInt16(Console.ReadLine());
+            estudiantes = new Estudiante[cantidadEstudiantes];
+
+            for (int i=0; i<cantidadEstudiantes; i++) {
+                
+                Estudiante miEstudiante = new Estudiante();
+                Console.WriteLine("===============estudiante #{0}===============", (i+1));
+                Console.WriteLine("Ingrese el nombre completo del estudiante: ");
+                miEstudiante.NombreCompleto = Console.ReadLine();
+                Console.WriteLine("Ingrese el N° cuenta: ");
+                miEstudiante.Cuenta = Console.ReadLine();
+                Console.WriteLine("Ingrese calificación del estudiante: ");
+                miEstudiante.CalificacionParcial = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                estudiantes[i] = miEstudiante;
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("===============Resultado===============");
+            int count = 1;
+            foreach (Estudiante estudianteActual in estudiantes) {
+                Console.WriteLine("===============estudiante #{0}===============",count++);
+                Console.WriteLine("Nombre: {0}", estudianteActual.NombreCompleto);
+                Console.WriteLine("Cuenta: {0}", estudianteActual.Cuenta);
+                Console.WriteLine("Calificación: {0}", estudianteActual.CalificacionParcial);
+                Console.WriteLine("Estado: {0}", estudianteActual.Estado);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Estudiante {
+        //Propiedades o Atributos
+        private string nombreCompleto;
+        private string cuenta;
+        private double calificacionParcial;
+        private string estado;
+
+        //Constructores
+        //Constructor por defecto
+        public Estudiante() { }
+
+        //Constructor con parametros
+        public Estudiante(string nombreCompleto, string cuenta, 
+            double calificacionParcial) {
+            NombreCompleto = nombreCompleto;
+            Cuenta = cuenta;
+            CalificacionParcial= calificacionParcial;
+        }
+
+        //Setters y Getters
+        public string NombreCompleto {
+            get {
+                return this.nombreCompleto;
+            }
+            set {
+                this.nombreCompleto = value; 
+            }
+        }
+        public string Cuenta
+        {
+            get {
+                return this.cuenta;
+            }
+            set { 
+                this.cuenta = value;
+            }
+        }
+
+        public double CalificacionParcial
+        {
+            get {
+                return this.calificacionParcial;
+            }
+            set {
+                if (value > 0) {
+                    if (value <60)
+                    {
+                        this.estado = "REPROBO";
+                    }
+                    else if (value > 60) {
+                        this.estado = "APROBO";
+                    }
+
+                    this.calificacionParcial = value;
+                }
+            }
+        }
+
+        public string Estado
+        {
+            get {
+                return this.estado;
+            }
+        }
+    }
+}
---
+++ classesandobjects/classesandobjects/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace figuras { 
+
+
+    public class Program { 
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Ingrese la altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el ancho: ");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+
+            Figura figura2 = new Figura();
+            Figura miFigura = new Figura(altura, ancho);
+
+            Console.WriteLine("El area del objeto es: {0}",miFigura.CalcularArea());
+            Console.WriteLine("El valor de altura objeto2 es: {0}",figura2.Altura);
+
+            
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Figura {
+        double altura;
+        double ancho;
+
+        public Figura() { }
+        public Figura(double altura, double ancho)
+        {
+            Altura = altura;
+            Ancho = ancho;
+        }
+
+
+        public double Altura
+        {
+            get {
+                return this.altura;
+            }
+            set {
+                this.altura = value;
+            }
+        }
+
+        public double Ancho
+        {
+            get {
+                return this.ancho;
+            }
+            set {
+                this.ancho = value;
+            }
+        }
+
+        public double CalcularArea() {
+            return Altura*Ancho;
+        }
+
+
+    }
+
+}
